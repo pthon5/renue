3e9d8450912c07f705a0c38648bb04b2a17549c8
pthon5 <8967858b87@gmail.com>
2021-07-26 17:10:48 +0500

    first

---
+++ src/main/java/com/pthon/renue/RenueApplication.java
@@ -0,0 +1,13 @@
+package com.pthon.renue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pthon/renue/RenueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pthon.renue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
